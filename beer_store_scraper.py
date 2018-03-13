2897213b8f3edf8bdcdf9d5b10a576b577662326
Colin Harrison <coharrison25@gmail.com>
2018-03-13 17:02:59 -0400

    Added writing Lager list and price per drink to Excel

--- beer_store_scraper.py
+++ beer_store_scraper.py
@@ -2,7 +2,8 @@
 # This program scrapes thebeerstore.ca for a list of url's to every beer
 # they sell. The beers are categorized into Ale, Lager, Malt & Stout.
 
-import requests, bs4, re
+import requests, bs4, re, openpyxl
+from openpyxl import Workbook
 
 # Refactor all of this into one function
 # So don't write same code four times, write one function
@@ -14,10 +15,6 @@ import requests, bs4, re
 linksRequest = requests.get('http://www.thebeerstore.ca/beers/search/beer_type--Lager')
 linksText = bs4.BeautifulSoup(linksRequest.text)
 rawLinks = linksText.select('.brand-link')
-
-#sizeRegex = re.compile(r'\d* ml')
-#canRegex = re.compile(r'473 ml')
-#kegRegex = re.compile(r'30000 ml')
 
 sizeRegex = re.compile(r'\d+.*ml')
 volumeRegex = re.compile(r'\d+\sml')
@@ -60,7 +57,51 @@ for i in range(len(rawLinks)):
         volume = volumeRegex.search(str(sizes[j])).group(0)
         quantity = quantityRegex.search(str(sizes[j])).group(0)
         size = sizeRegex.search(str(sizes[j])).group(0)
-        lagerList.append([name, size, quantity, volume, ABV, price])
-        print([name, size, quantity, volume, ABV, price])
+        lagerList.append([name, size, quantity, volume[:-3], ABV, price]) # remove ' ml'
+        #print([name, size, quantity, volume, ABV, price])
 
-# TODO: Write lagerList to an Excel Spreadsheet
+wb = Workbook()
+ws = wb.active
+ws.title = 'Lagers'
+
+# Set headers
+ws['B3'] = 'Beer'
+ws['C3'] = 'Sold as'
+ws['D3'] = 'Quantity'
+ws['E3'] = 'Volume (ml)'
+ws['F3'] = 'ABV'
+ws['G3'] = 'Price'
+ws['H3'] = 'Price per Drink'
+ws.column_dimensions['B'].width = 24
+ws.column_dimensions['C'].width = 20
+ws.column_dimensions['H'].width = 12
+
+row = 4
+mL_per_drink = 17.7441 # ml
+
+for i in range(len(lagerList)):
+    nameIndex = 'B' + str(row)
+    sizeIndex = 'C' + str(row)
+    quantityIndex = 'D' + str(row)
+    volIndex = 'E' + str(row)
+    abvIndex = 'F' + str(row)
+    priceIndex = 'G' + str(row)
+    costPerDrinkIndex = 'H' + str(row)
+    row += 1
+
+    # Import list into Excel
+    ws[nameIndex] = lagerList[i][0]
+    ws[sizeIndex] = lagerList[i][1]
+    ws[quantityIndex] = int(lagerList[i][2])
+    ws[volIndex] = float(lagerList[i][3])
+    ws[abvIndex] = lagerList[i][4]
+    ws[priceIndex] = float(lagerList[i][5])
+    
+    # Get price per drink (17.7441 mL alcohol)
+    alcoholVol = float(lagerList[i][2]) * float(lagerList[i][3]) * float(lagerList[i][4])
+    dollars_per_mL = float(lagerList[i][5]) / alcoholVol
+    costPerDrink = dollars_per_mL * mL_per_drink
+    ws[costPerDrinkIndex] = round(costPerDrink,2)
+    
+
+wb.save('beer_store_catalog.xlsx')
