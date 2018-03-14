2b54832040d2bbdf69f6d423e22d52d391b3972e
Colin Harrison <coharrison25@gmail.com>
2018-03-13 22:30:22 -0400

    Fixed parser warning

--- beer_store_scraper.py
+++ beer_store_scraper.py
@@ -2,15 +2,16 @@
 # This program scrapes thebeerstore.ca for a list of url's to every beer
 # they sell. The beers are categorized into Ale, Lager, Malt & Stout.
 
-import requests, bs4, re, openpyxl
+import requests, bs4, re, openpyxl, lxml
 from openpyxl import Workbook
+from openpyxl.styles import Font
 
 def scrape(beerType):
 
     # Get all links for the type of beer
     url = 'http://www.thebeerstore.ca/beers/search/beer_type--' + beerType
     linksRequest = requests.get(url)
-    linksText = bs4.BeautifulSoup(linksRequest.text)
+    linksText = bs4.BeautifulSoup(linksRequest.text, "lxml")
     rawLinks = linksText.select('.brand-link')
 
     # Regular expression declarations
@@ -29,7 +30,7 @@ def scrape(beerType):
         match = linkRegex.search(rawLinksString)
         fullLink = 'http://www.thebeerstore.ca' + match.group(0)
         beerInfo = requests.get(fullLink)
-        beerSoup = bs4.BeautifulSoup(beerInfo.text)
+        beerSoup = bs4.BeautifulSoup(beerInfo.text, "lxml")
 
         # Get prices and sizes of the beer
         prices = beerSoup.select('.price')
@@ -62,6 +63,7 @@ maltList = scrape('Malt')
 stoutList = scrape('Stout')
 beerLists = [lagerList, aleList, maltList, stoutList]
 
+# Create 4 worksheets
 wb = Workbook()
 ws = wb.active
 ws.title = 'Lagers'
@@ -69,8 +71,7 @@ wb.create_sheet(title='Ales')
 wb.create_sheet(title='Malts')
 wb.create_sheet(title='Stouts')
 
-
-
+bold11Font = Font(size=11, bold=True)
 mL_per_drink = 17.7441 # ml
 
 for counter, beerList in enumerate(beerLists):
@@ -79,12 +80,21 @@ for counter, beerList in enumerate(beerLists):
     # Set headers and change active sheet
     ws = wb[beerTypes[counter]]
     ws['B3'] = 'Beer'
+    ws['B3'].font = bold11Font
     ws['C3'] = 'Sold as'
+    ws['C3'].font = bold11Font
     ws['D3'] = 'Quantity'
+    ws['D3'].font = bold11Font
     ws['E3'] = 'Volume (ml)'
+    ws['E3'].font = bold11Font
     ws['F3'] = 'ABV'
+    ws['F3'].font = bold11Font
     ws['G3'] = 'Price'
+    ws['G3'].font = bold11Font
+    ws['G3'].number_format = '$#.##'
     ws['H3'] = 'Price per Drink'
+    ws['H3'].font = bold11Font
+    ws['G3'].number_format = '$#.##'
     ws.column_dimensions['B'].width = 24
     ws.column_dimensions['C'].width = 20
     ws.column_dimensions['H'].width = 12
