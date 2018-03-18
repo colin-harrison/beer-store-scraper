eccc108cc1043f91d1ef6c9391f44bd7b1ddb906
Colin Harrison <coharrison25@gmail.com>
2018-03-17 22:44:01 -0400

    Added simple insertion sort implementation

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,12 +3,13 @@
 # of insertion sort.
 
 def sort(beerList):
+    print('we sorting')
     for i in range(len(beerList)):
-        for j in range(i):
+        for j in range(i, 0, -1):
             if beerList[j-1][6] > beerList[j][6]:
-                temp = beerList[j-1][6]
-                beerList[j-1][6] = beerList[j][6]
-                beerList[j][6] = temp
+                temp = beerList[j-1]
+                beerList[j-1] = beerList[j]
+                beerList[j] = temp
             else:
                 break
     return beerList
